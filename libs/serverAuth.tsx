8a42acc6f071bd8aef262115f9e6d00a7d3dd5ee
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-04-17 20:08:42 +0530

    create registeruser api and function to check if user is signed in or not

---
+++ libs/serverAuth.tsx
@@ -0,0 +1,24 @@
+//this function checks if the user is signed in and retrieves the current user's information from the database using Prisma.
+
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
+import prisma from "@/libs/prismadb";
+
+const serverAuth = async (req: NextApiRequest) => {
+     const session = await getSession({ req });
+
+     if (!session?.user) {
+          throw new Error("not signed in");
+     }
+     const currentUser = await prisma.user.findUnique({
+          where: {
+               email: session.user.email,
+          },
+     });
+     if (!currentUser) {
+          throw new Error("Not signed in");
+     }
+     return { currentUser };
+};
+
+export default serverAuth;
